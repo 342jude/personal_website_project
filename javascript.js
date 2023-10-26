7d1d39f7eaed3f47d44616ecd82065418374a8cb
judek <kiguthadeus@gmail.com>
2023-10-25 21:31:56 -0400

    new javascript

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
-=======
-Hello World
+
 console.log("Hello World")
 alert("Hello World")
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent the form from submitting to a server
+
+    // Do any form validation if required...
+
+    document.getElementById('successMessage').removeAttribute('hidden');
+});
